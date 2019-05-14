04489614a2463aae413d7850a54b3ec85fb42c50
Aleesia <homtom.alice.2016@gmail.com>
2019-05-14 16:02:56 +0300

    Alisa_uses_git

---
+++ src/Tree.h
@@ -0,0 +1,332 @@
+#ifndef TREE_MOSKANOVA_TREE_H
+#define TREE_MOSKANOVA_TREE_H
+#include<iostream>
+#include<cstring>
+#include<string.h>
+#include<typeinfo>
+#define MAX_TREE_DEPTH 50
+#define STR_LEN 50
+#define OUTPUT_STR "        "
+
+template<typename nodeType>
+class TreeNode{
+    nodeType data;
+    TreeNode *parent;
+    TreeNode** children;
+    unsigned number_of_children;
+    static char output_str[STR_LEN];
+    TreeNode<nodeType>* recursive_copy_subtree(TreeNode<nodeType> *copy_of_parent_node); //alisa func
+
+public:
+    explicit TreeNode(nodeType node_data);
+    TreeNode();
+    TreeNode(const TreeNode<nodeType> &node);
+    ~TreeNode();
+
+    // Node data ops
+    nodeType get_data();
+    void set_data(nodeType new_data);
+
+    // Parent ops
+    TreeNode* get_parent();
+    void set_parent(const TreeNode<nodeType> &new_parent);
+    TreeNode** get_parents(); // full list of parents
+    nodeType get_parent_data();
+    nodeType* get_parents_data();
+
+    // Children ops
+    void add_child(const TreeNode<nodeType> &child);
+    void add_child(TreeNode<nodeType> *child);
+    nodeType *get_children_data();
+    unsigned get_number_of_children();
+
+    void output();                      //alisa func
+
+    TreeNode<nodeType> *copy();        //alisa func
+    TreeNode<nodeType> *copy_subtree(); //alisa func
+
+    // Show section
+
+    //    void show(); // shows node data & stuff
+
+};
+
+template<typename nodeType>
+char TreeNode<nodeType>::output_str[STR_LEN]="";
+//============== NODE CONSTRUCTORS ===============
+
+template <typename nodeType>
+TreeNode<nodeType>::TreeNode(nodeType node_data) {
+    data = node_data;
+    parent = nullptr;
+    children = nullptr;
+    number_of_children = 0;
+}
+
+template <typename nodeType>
+TreeNode<nodeType>::TreeNode() {
+    if (typeid(nodeType).name() == typeid(std::string).name())
+    {
+        data = '\0';
+    }
+    else
+    {
+        data = (nodeType) 0;
+    }
+    children = nullptr;
+    parent = nullptr;
+    number_of_children = 0;
+}
+
+//============== NODE DESTRUCTOR ===============
+
+template <typename nodeType>
+TreeNode<nodeType>::~TreeNode()
+{
+    if (number_of_children > 0)
+    {
+        for (int i = 0; i < number_of_children; ++i) {
+            delete children[i];
+        }
+        delete [] children;
+        number_of_children = 0; //without this i had segmentation fault
+    }
+    parent = nullptr;
+}
+
+//================== NODE COPY =================
+template<typename nodeType>               //alisa func
+TreeNode<nodeType>::TreeNode(const TreeNode<nodeType> &node)
+{
+    this->data = node.data;
+    this->number_of_children = 0;
+    this->parent = node.parent;
+    this->children = nullptr;
+}
+//================== DATA OPS ==================
+
+template <typename nodeType>
+nodeType TreeNode<nodeType>::get_data() {
+    return data;
+}
+
+template <typename nodeType>
+void TreeNode<nodeType>::set_data(nodeType new_data) {
+    data = new_data;
+}
+
+//================= PARENT OPS =================
+
+template <typename nodeType>
+TreeNode<nodeType>* TreeNode<nodeType>::get_parent() {
+    return parent;
+}
+
+template <typename nodeType>
+void TreeNode<nodeType>::set_parent(const TreeNode<nodeType> &new_parent) {
+    if (parent!=NULL){
+        parent->remove_child(*this);
+    }
+    parent = *new_parent;
+}
+
+template <typename nodeType>
+TreeNode<nodeType>** TreeNode<nodeType>::get_parents() {
+    if (this->parent == nullptr) {
+        return nullptr;
+    }
+
+    TreeNode<nodeType>** parents;
+    parents = new TreeNode<nodeType>*[MAX_TREE_DEPTH];
+
+    unsigned parents_len = 0;
+    TreeNode<nodeType>* currentNode = this;
+    while (currentNode->parent != nullptr) {
+        parents_len++;
+        currentNode = this->parent;
+
+        parents[parents_len - 1] = currentNode;
+    }
+
+    TreeNode<nodeType> **parents1 = new TreeNode<nodeType>[parents_len];
+    for (int i = 0; i < parents_len; ++i) {
+        parents1[i] = parents[i]; //todo hope this works
+    }
+    delete[] parents;
+
+    return parents1;
+}
+
+template <typename nodeType>
+nodeType TreeNode<nodeType>::get_parent_data() {
+    if (parent==nullptr) {return (nodeType)0;}
+    else {return parent->data;}
+}
+
+template <typename nodeType>
+nodeType* TreeNode<nodeType>::get_parents_data() {
+    if (this->parent == nullptr) {
+        return nullptr;
+    }
+
+    nodeType* parents_data;
+    parents_data = new nodeType[MAX_TREE_DEPTH];
+
+    unsigned parents_len = 0;
+    TreeNode<nodeType>* currentNode = this;
+    while (currentNode->parent != nullptr) {
+        parents_len++;
+        currentNode = this->parent;
+
+        parents_data[parents_len - 1] = currentNode->data;
+    }
+
+    auto parents_data1 = new nodeType[parents_len];
+    for (int i = 0; i < parents_len; ++i) {
+        parents_data1[i] = parents_data[i]; //todo hope this works
+    }
+    delete[] parents_data;
+
+    return parents_data1;
+}
+
+//================ CHILDREN OPS ================
+
+template <typename nodeType>
+void TreeNode<nodeType>::add_child(const TreeNode<nodeType> &child) {
+    if (number_of_children > 0) {
+        auto new_children = new TreeNode<nodeType> *[number_of_children + 1];
+        for (int i = 0; i < number_of_children; ++i) {
+            new_children[i] = children[i];
+        }
+        new_children[number_of_children] = *child;//&child
+        number_of_children++;
+    } else {
+        number_of_children++;
+        children = new TreeNode [number_of_children];
+        children[0] = *child;
+    }
+    child.parent = this;
+}
+
+template<typename nodeType>
+void TreeNode<nodeType>::add_child(TreeNode<nodeType>*child){
+    if (number_of_children>0)
+    {
+        auto new_children = new TreeNode<nodeType>*[number_of_children+1];
+        for (int i=0; i<number_of_children; ++i){
+            new_children[i] = children[i];
+        }
+        new_children[number_of_children] = child;
+        number_of_children++;
+        delete[] children;
+        children = new_children;
+    } else{
+        number_of_children++;
+        children = new TreeNode*[number_of_children];
+        children[0] = child;
+    }
+    child->parent = this;
+}
+
+template <typename nodeType>
+unsigned TreeNode<nodeType>::get_number_of_children() {
+    return number_of_children;
+}
+
+template <typename nodeType>
+nodeType* TreeNode<nodeType>::get_children_data() {
+    if (children==nullptr) {return nullptr;}
+    auto children_data = new nodeType[number_of_children]; // a pointer :)
+    for (int i = 0; i < number_of_children; i++) {
+        children_data[i] = children[i]->data;
+    }
+    return children_data;
+}
+
+
+//============= MELEKESTSEVA'S CODING ==============
+
+template<typename nodeType>
+void TreeNode<nodeType>::output()
+{
+    if (output_str[strlen(output_str)-1]=='|')
+    {
+        std::cout<<this->output_str<<"---<"<<this->data<<'>'<<std::endl;
+    }
+    else {
+        std::cout << this->output_str << "|---<" << this->data << '>' << std::endl;
+    }
+    if (number_of_children) {
+        strcat(output_str, OUTPUT_STR);
+        strcat(output_str, "|");
+        for (unsigned i = 0; i < number_of_children - 1; ++i) {
+            children[i]->output();
+        }
+        unsigned l = strlen(output_str);
+        output_str[l - 9] = '\0';
+        strcat(output_str, OUTPUT_STR);
+        children[number_of_children - 1]->output();
+        l = strlen(output_str);
+        output_str[l - 8] = '\0';
+    }
+}
+
+
+template<typename nodeType>
+TreeNode<nodeType> *TreeNode<nodeType>::copy()
+{
+    TreeNode<nodeType>* new_node = new TreeNode<nodeType>;
+    new_node->data = data;
+    if (parent==nullptr) {new_node->parent = nullptr;}
+    else
+        {new_node->parent = parent;
+        parent->add_child(new_node);
+        };
+    new_node->number_of_children = 0;
+    new_node->children = nullptr;
+    return new_node;
+}
+
+template<typename nodeType>
+TreeNode<nodeType>* TreeNode<nodeType>::recursive_copy_subtree(TreeNode<nodeType> *copy_of_parent_node)
+{
+    TreeNode<nodeType> *node_copy = new TreeNode<nodeType>;
+    node_copy->data = this->data;
+    node_copy->parent = copy_of_parent_node;
+    node_copy->number_of_children = this->number_of_children;
+    if (number_of_children)
+    {
+        node_copy->children = new TreeNode<nodeType>* [number_of_children];
+        for (unsigned i=0; i<number_of_children; i++)
+        {
+            node_copy->children[i] = this->children[i]->recursive_copy_subtree(node_copy);
+        }
+    }
+    return node_copy;
+}
+
+template<typename nodeType>
+TreeNode<nodeType> *TreeNode<nodeType>::copy_subtree() //this does NOT work:( SIGSEGV
+{
+    TreeNode<nodeType> *new_node = new TreeNode<nodeType>;
+    new_node->data = data;
+    new_node->parent = NULL;
+    new_node->number_of_children = number_of_children;
+    if (number_of_children>0)
+    {
+        new_node->children = new TreeNode<nodeType>* [number_of_children];
+        for (int i=0; i<number_of_children; ++i)
+        {
+            new_node->children[i] = children[i]->recursive_copy_subtree(new_node);
+        }
+    }
+    else
+    {
+        new_node->children = nullptr;
+    }
+    return new_node;
+}
+
+
+#endif //TREE_MOSKANOVA_TREE_H
---
+++ tests/test_tree.cpp
@@ -0,0 +1,86 @@
+#include <iostream>
+#include "Tree.h"
+#include<cstring>
+
+using namespace std;
+void create_projects_tree()
+{
+    TreeNode<std::string> str_root("project");
+    TreeNode<std::string> str_child_11("project_C");
+    TreeNode<std::string> str_child_12("project_C++");
+
+    TreeNode<std::string> str_child_21("GeomND");
+    TreeNode<std::string> str_child_22("Spline");
+    TreeNode<std::string> str_child_23("BigNumber");
+    TreeNode<std::string> str_child_24("Interpolation");
+
+    TreeNode<std::string> str_child_31("Tenzor");
+    TreeNode<std::string> str_child_32("Tree");
+    TreeNode<std::string> str_child_33("Graph");
+    TreeNode<std::string> str_child_34("Bank");
+
+    TreeNode<std::string> str_child_41("Moskanova");
+    TreeNode<std::string> str_child_42("Melekestseva");
+    TreeNode<std::string> str_child_43("Horbunov");
+    TreeNode<std::string> str_child_44("Kovaltchuk");
+    TreeNode<std::string> str_child_45("Stoychik");
+    TreeNode<std::string> str_child_46("Prygodiuk");
+
+    str_child_21.add_child(&str_child_45);
+    str_child_21.add_child(&str_child_42);
+    str_child_22.add_child(&str_child_43);
+    str_child_22.add_child(&str_child_44);
+    str_child_24.add_child(&str_child_41);
+    str_child_31.add_child(&str_child_46);
+
+    str_root.add_child(&str_child_11);
+    str_root.add_child(&str_child_12);
+    str_child_11.add_child(&str_child_21);
+    str_child_11.add_child(&str_child_22);
+    str_child_11.add_child(&str_child_23);
+    str_child_11.add_child(&str_child_24);
+    str_child_12.add_child(&str_child_31);
+    str_child_12.add_child(&str_child_32);
+    str_child_12.add_child(&str_child_33);
+    str_child_12.add_child(&str_child_34);
+    str_root.output();
+    std::cout<<std::endl;
+
+    auto *copy_projects = str_child_11.copy_subtree();
+    copy_projects->output();
+}
+
+int main()
+{
+    auto *root = new TreeNode<float>(9.99);
+    TreeNode<float> ch(1);
+    root->add_child(&ch);
+    TreeNode<float> ch_1(22);
+    ch.add_child(&ch_1);
+    TreeNode<float> ch_3(44);
+    TreeNode<float> ch_5(55);
+    ch.add_child(&ch_3);
+    ch.add_child(&ch_5);
+
+    TreeNode<float>* new_tree = new TreeNode<float>;
+    new_tree->add_child(root);
+    TreeNode<float> left_1(8);
+    new_tree->add_child(&left_1);
+    TreeNode<float> middle(33);
+    new_tree->add_child(&middle);
+    new_tree->output();
+
+    std::cout<<"\n\n";
+    //create_projects_tree();
+     TreeNode<float> *test_node = new TreeNode<float>(4567);
+     test_node = root->copy();
+
+     new_tree->output();
+     std::cout<<std::endl;
+
+     test_node = root->copy_subtree();
+     test_node->output();
+     std:cout<<std::endl;
+
+     create_projects_tree();
+}
